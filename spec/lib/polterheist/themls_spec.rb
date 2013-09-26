ee8ef2760af4e0cd7660db7bacdf958f0028ceaa
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-09-25 17:29:45 -0700

    Success

--- spec/lib/polterheist/themls_spec.rb
+++ spec/lib/polterheist/themls_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 
 describe Polterheist::TheMLS do
   describe "#locate_property" do
-    context "when no homes are found" do
+    let(:result) { subject.locate_property address }
+
+    context "not located" do
       let(:address) { "8622 LEHIGH Ave" }
+      
       it "returns false" do
-        res = subject.locate_property "8622 LEHIGH Ave"
-        res.should be_false
+        result.should be_false
+      end
+    end
+
+    context "located" do
+      let(:address) { "7654 WESTLAND Ave" }
+
+      it "can be further parsed" do
+        doc = Nokogiri::HTML.parse(result)
+        img_src = doc.css(".mainImage img").first.attr :src
+        img_src.should match /(jpg|png)/
       end
     end
   end
